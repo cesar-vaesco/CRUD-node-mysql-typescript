9df48b8f53436d5cf036cc649c5eb429af736271
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-08-06 08:48:36 -0500

    Middleware necesarios para empezar a correr la aplicación

--- models/server.ts
+++ models/server.ts
@@ -1,12 +1,13 @@
 import express, { Application } from 'express';
 
 import userRoutes from '../routes/usuario';
+import cors from 'cors';
 
 class Server {
 
     private app: Application;
     private port: string;
-    private apiPath ={
+    private apiPath = {
         usuarios: '/api/usuarios'
     }
 
@@ -14,13 +15,29 @@ class Server {
         this.app = express();
         this.port = process.env.PORT || '8080';
 
+
+        //Métodos iniciales
+        this.middlewares();
         //Definir mis rutas
         this.routes();
     }
 
+    middlewares() {
 
-    routes(){
-        this.app.use( this.apiPath.usuarios, userRoutes );
+        //CORS middle
+        this.app.use(cors());
+
+        //Lectura del body
+        this.app.use(express.json());
+
+        //Carpeta publicas
+        this.app.use(express.static('public'));
+
+    }
+
+
+    routes() {
+        this.app.use(this.apiPath.usuarios, userRoutes);
     }
 
 
